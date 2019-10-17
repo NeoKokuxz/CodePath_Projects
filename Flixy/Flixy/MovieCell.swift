5f2d117144775419e6185761e952503944713104
NeoKokuxz <neko.kokubyakuin@yahoo.com>
2019-10-17 10:16:33 -0400

    first commit for flixy

---
+++ Flixy/Flixy/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flixy
+//
+//  Created by Naoki on 10/16/19.
+//  Copyright © 2019 Naoki. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var movieImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
