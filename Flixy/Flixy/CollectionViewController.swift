4ba5213a5ebc7f23354f39ac3fc186e7c5b59443
NeoKokuxz <neko.kokubyakuin@yahoo.com>
2019-11-08 19:36:13 -0500

    updated

---
+++ Flixy/Flixy/CollectionViewController.swift
@@ -0,0 +1,103 @@
+//
+//  CollectionViewController.swift
+//  Flixy
+//
+//  Created by Naoki on 10/24/19.
+//  Copyright © 2019 Naoki. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var movies = [[String:Any]]()
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        //layout config
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 9  
+        layout.minimumInteritemSpacing = 0
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2)  / 2
+            
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+        // Do any additional setup after loading the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+              // TODO: Get the array of movies
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+
+            self.collectionView.reloadData()
+            }
+        }
+        task.resume()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        
+        let movie = movies[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.poster.af_setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        //Find the movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movieTap = movies[indexPath.row]
+        
+        //pass the selected movie
+        let infoViewController = segue.destination as! InfoViewController
+        
+        infoViewController.movieSelect = movieTap
+        
+//        collectionView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+    
+}
---
+++ Flixy/README.md
@@ -0,0 +1,49 @@
+# Flixy
+
+<<<<<<< Updated upstream
+Flixy is an app that allows users to browse movies from the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+=======
+Flix is an app that allows users to browse movies from the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+
+
+## Flix Part 2
+
+### User Stories
+
+#### REQUIRED (10pts)
+- [ ] (5pts) User can tap a cell to see more details about a particular movie.
+- [ ] (5pts) User can tap a tab bar button to view a grid layout of Movie Posters using a CollectionView.
+
+#### BONUS
+- [ ] (2pts) User can tap a poster in the collection view to see a detail screen of that movie.
+- [ ] (2pts) In the detail view, when the user taps the poster, a new screen is presented modally where they can view the trailer.
+
+### App Walkthough GIF
+`TODO://` Add the URL to your animated app walkthough `gif` in the image tag below, `YOUR_GIF_URL_HERE`. Make sure the gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after after adding gif)
+
+<img src="YOUR_GIF_URL_HERE" width=250><br>
+
+### Notes
+Describe any challenges encountered while building the app.
+>>>>>>> Stashed changes
+
+---
+
+## Flixy Part 1
+
+### User Stories
+#### REQUIRED (10pts)
+- [x] (2pts) User sees an app icon on the home screen and a styled launch screen.
+- [x] (5pts) User can view and scroll through a list of movies now playing in theaters.
+- [x] (3pts) User can view the movie poster image for each movie.
+
+#### BONUS
+- [x] (2pt) User can view the app on various device sizes and orientations.
+- [x] (1pt) Run your app on a real device.
+
+### App Walkthough GIF
+<img src="http://g.recordit.co/J7wRRjhjyV.gif" width=250><br>
+
+### Note
+
+
